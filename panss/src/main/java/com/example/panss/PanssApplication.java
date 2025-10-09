f3ddfc87727af468fb69735570863b4d0217d6ec
RiteshPandey2003 <riteshpandey72060@gmail.com>
2025-10-09 22:01:48 +0530

    first comment

---
+++ panss/src/main/java/com/example/panss/PanssApplication.java
@@ -0,0 +1,13 @@
+package com.example.panss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanssApplication.class, args);
+	}
+
+}
---
+++ panss/src/test/java/com/example/panss/PanssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.panss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
